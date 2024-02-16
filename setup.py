828b4c47822ff3efa5c9ef445369e6eb0d7bb7b5
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-02-16 16:08:34 +0100

    prepare for release

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+PY_TGI_VERSION = "0.1.0"
+
+common_setup_kwargs = {
+    "author": "Ilyas Moutawwakil",
+    "author_email": "ilyas.moutawwakil@gmail.com",
+    "description": "A Python wrapper around TGI",
+    "keywords": ["python", "tgi", "llm", "huggingface", "docker"],
+    "url": "https://github.com/IlyasMoutawwakil/py-tgi",
+    "long_description_content_type": "text/markdown",
+    "long_description": (Path(__file__).parent / "README.md").read_text(
+        encoding="UTF-8"
+    ),
+    "platforms": ["linux", "windows", "macos"],
+    "classifiers": [
+        "Environment :: GPU :: NVIDIA CUDA :: 11.8",
+        "Environment :: GPU :: NVIDIA CUDA :: 12",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+    ],
+}
 
 setup(
     name="py-tgi",
-    version="0.0.1",
+    version=PY_TGI_VERSION,
     packages=find_packages(),
     install_requires=["docker", "huggingface-hub"],
 )
