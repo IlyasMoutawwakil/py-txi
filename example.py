8a36542e2b8090db148553955eaa354d0580ef57
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2024-02-20 16:22:55 +0000

    update example

--- example.py
+++ example.py
@@ -1,29 +1,15 @@
 from py_tgi import TGI
 from py_tgi.utils import is_nvidia_system, is_rocm_system
 
-if is_rocm_system():
+if is_rocm_system() or is_nvidia_system():
     llm = TGI(
-        model="TheBloke/Llama-2-7B-AWQ",  # model name
-        image="ghcr.io/huggingface/text-generation-inference:latest-rocm",  # rocm image
-        devices=["/dev/kfd", "/dev/dri"],  # all rocm devices
-        quantize="gptq",  # use exllama kernels
+        model="TheBloke/Llama-2-7B-AWQ",  # awq model checkpoint
+        devices=["/dev/kfd", "/dev/dri"] if is_rocm_system() else None,  # custom devices (ROCm)
+        gpus="all" if is_nvidia_system() else None,  # all gpus (NVIDIA)
+        quantize="gptq",  # use exllama kernels (rocm compatible)
     )
-    output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
-    print(output)
-
-elif is_nvidia_system():
-    llm = TGI(
-        model="TheBloke/Llama-2-7B-AWQ",  # model name
-        quantize="gptq",  # use exllama kernels
-        gpus="all",  # all gpus
-    )
-    output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
-    print(output)
-
 else:
-    llm = TGI(
-        model="gpt2",  # model name
-        sharded=False,  # disable sharding on cpu
-    )
-    output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
-    print(output)
+    llm = TGI(model="gpt2", sharded=False)
+
+output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
+print(output)
