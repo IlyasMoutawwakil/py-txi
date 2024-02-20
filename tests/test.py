3a76fe299c05d8871f4ca4a69fce8c2d3ae41668
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2024-02-20 16:02:31 +0000

    update test

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,6 @@
 from py_tgi import TGI
-from py_tgi.utils import is_nvidia_system, is_rocm_system
 
-llm = TGI(
-    "gpt2",
-    sharded=False,
-    gpus="0,1" if is_nvidia_system() else None,
-    devices=["/dev/kfd", "/dev/dri"] if is_rocm_system() else None,
-)
+llm = TGI("gpt2", gpus=None, devices=None, sharded=False)
 
 output = llm.generate("Hi, I'm a sanity test")
 assert isinstance(output, str)
