7f416ffb91118f7d7cebc083df026d07a4c12872
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-02-22 15:57:36 +0100

    update test

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,15 @@
-from py_tgi import TGI
-from py_tgi.utils import is_nvidia_system, is_rocm_system
+import numpy as np
 
-if is_nvidia_system() or is_rocm_system():
-    llm = TGI(
-        model="TheBloke/Llama-2-7B-AWQ",  # awq model checkpoint
-        devices=["/dev/kfd", "/dev/dri"] if is_rocm_system() else None,  # custom devices (ROCm)
-        gpus="all" if is_nvidia_system() else None,  # all gpus (NVIDIA)
-        quantize="gptq",  # use exllama kernels (rocm compatible)
-    )
-    output = llm.generate("Hi, I'm a sanity test")
-    assert isinstance(output, str)
-    output = llm.generate(["Hi, I'm a sanity test", "I'm a second sentence"])
-    assert isinstance(output, list) and all(isinstance(x, str) for x in output)
+from py_tgi import TEI, TGI
 
-else:
-    llm = TGI(model="gpt2", sharded=False)
-    output = llm.generate("Hi, I'm a sanity test")
-    assert isinstance(output, str)
-    output = llm.generate(["Hi, I'm a sanity test", "I'm a second sentence"])
-    assert isinstance(output, list) and all(isinstance(x, str) for x in output)
+embed = TEI(model="bert-base-uncased", dtype="float16", pooling="mean", port=1234)
+output = embed.encode("Hi, I'm a language model")
+assert isinstance(output, np.ndarray)
+output = embed.encode(["Hi, I'm a language model", "I'm fine, how are you?"])
+assert isinstance(output, list) and all(isinstance(x, np.ndarray) for x in output)
+
+llm = TGI(model="gpt2", sharded=False, port=4321)
+output = llm.generate("Hi, I'm a sanity test")
+assert isinstance(output, str)
+output = llm.generate(["Hi, I'm a sanity test", "I'm a second sentence"])
+assert isinstance(output, list) and all(isinstance(x, str) for x in output)
