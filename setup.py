4a0ac0926e7f8049c5c3a4985e4db8d78f916b48
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-02-22 16:38:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-PY_TGI_VERSION = "0.1.3"
+PY_TGI_VERSION = "0.2.0"
 
 common_setup_kwargs = {
     "author": "Ilyas Moutawwakil",
     "author_email": "ilyas.moutawwakil@gmail.com",
-    "description": "A Python wrapper around TGI",
-    "keywords": ["python", "tgi", "llm", "huggingface", "docker"],
+    "description": "A Python wrapper around TGI and TEI servers",
+    "keywords": ["tgi", "llm", "tei", "embedding", "huggingface", "docker", "python"],
     "url": "https://github.com/IlyasMoutawwakil/py-tgi",
     "long_description_content_type": "text/markdown",
     "long_description": (Path(__file__).parent / "README.md").read_text(encoding="UTF-8"),
