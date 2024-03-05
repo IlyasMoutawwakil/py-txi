edf2a4e4aa29f25a8ec71685da42d83a19789781
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2024-03-05 07:26:28 +0100

    fix

--- py_txi/__init__.py
+++ py_txi/__init__.py
@@ -1,3 +1,3 @@
-from .text_embedding_inference import TEI  # noqa
-from .text_generation_inference import TGI  # noqa
+from .text_embedding_inference import TEI, TEIConfig  # noqa
+from .text_generation_inference import TGI, TGIConfig  # noqa
 from .utils import is_nvidia_system, is_rocm_system, get_free_port  # noqa
