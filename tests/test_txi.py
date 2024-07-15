a314403934a60050aa12512039d74c52383c68fb
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2024-07-15 10:50:53 +0200

    Update test_txi.py

--- tests/test_txi.py
+++ tests/test_txi.py
@@ -5,7 +5,7 @@ from py_txi import TEI, TGI, TEIConfig, TGIConfig
 
 def test_cpu_tei():
     embed = TEI(config=TEIConfig(model_id="BAAI/bge-base-en-v1.5"))
-    output = embed.encode("Hi, I'm a language model")
+    output = embed.encode("Hi, I'm a language model ")
     assert isinstance(output, np.ndarray)
     output = embed.encode(["Hi, I'm a language model", "I'm fine, how are you?"])
     assert isinstance(output, list) and all(isinstance(x, np.ndarray) for x in output)
