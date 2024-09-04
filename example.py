163777ee543943887291821bdb1434bf8c0c771a
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-09-04 10:49:13 +0200

    misc updates

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
 from py_txi.text_embedding_inference import TEI, TEIConfig
 from py_txi.text_generation_inference import TGI, TGIConfig
 
-llm = TGI(config=TGIConfig(model_id="bigscience/bloom-560m", gpus="0"))
-output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
-print(len(output))
-print("LLM:", output)
-llm.close()
+for gpus in [None, "1", "1,2"]:
+    llm = TGI(config=TGIConfig(model_id="bigscience/bloom-560m", gpus=gpus))
+    output = llm.generate(["Hi, I'm a language model", "I'm fine, how are you?"])
+    print(len(output))
+    print("LLM:", output)
+    llm.close()
 
-embed = TEI(config=TEIConfig(model_id="BAAI/bge-base-en-v1.5"))
-output = embed.encode(["Hi, I'm an embedding model", "I'm fine, how are you?"])
-print(len(output))
-print("Embed:", output)
-embed.close()
+    embed = TEI(config=TEIConfig(model_id="BAAI/bge-base-en-v1.5", gpus=gpus))
+    output = embed.encode(["Hi, I'm an embedding model", "I'm fine, how are you?"])
+    print(len(output))
+    print("Embed:", output)
+    embed.close()
